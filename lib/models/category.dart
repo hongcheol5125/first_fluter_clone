a696ddaef6453f4d7c9638397c67c644ee0f6de3
kisankang <rkdrltks@gmail.com>
2023-02-16 17:05:17 +1030

    fixed category

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,14 +1,53 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:hong/helpers/iconhelper.dart';
 import 'package:hong/models/subcategory.dart';
 
 class Category {
-  String? name;
-  String? icon;
-  Color? color;
-  String? imgName;
+  String name;
+  String icon;
+  Color color;
+  String imgName;
   List<SubCategory>? subCategories;
 
-  Category(
-      {this.name, this.icon, this.color, this.imgName, this.subCategories});
+  Category({
+    required this.name,
+    required this.icon,
+    required this.color,
+    required this.imgName,
+    this.subCategories,
+  });
+}
+
+class TestClass {
+  Category mockData = Category(
+    name: "mockData",
+    imgName: "japan1",
+    color: Colors.red,
+    icon: IconfontHelper.leaf,
+  );
+  method() {
+    mockData.name;
+    print(mockData.icon);
+  }
+
+  User hong = User(
+    age: 1,
+    name: "hong",
+  );
+  User kisan = User(age: 1, name: "kisan");
+}
+
+class User {
+  int age;
+  String name;
+  late String gender;
+  User({required this.age, required this.name, bool isMan = true}) {
+    if (isMan = true) {
+      this.gender = "M";
+    } else {
+      this.gender = "F";
+    }
+  }
 }
--- lib/models/subcategory.dart
+++ lib/models/subcategory.dart
@@ -1,17 +1,16 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:hong/models/categorypart.dart';
 
 import 'category.dart';
 
 class SubCategory extends Category {
-
   List<CategoryPart> parts;
-SubCategory({
-  required this.parts,
-  super.name,
-  super.icon,
-  super.color,
-  super.imgName
-});
+  SubCategory(
+      {required this.parts,
+      super.name = "null",
+      super.icon = "null",
+      super.color = Colors.red,
+      super.imgName = "null"});
 }
